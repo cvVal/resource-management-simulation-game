3ef16213b99b0a1553096823c98c1de5a7fe7aa9
cvVal <cvvaldez_00@hotmail.com>
2025-07-18 20:25:31 -0400

    add productivity unit

---
+++ Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    private ResourcePile m_CurrentPile;
+    public float ProductivityMultiplier = 2f;
+
+    protected override void BuildingInRange()
+    {
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+    }
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+
+    public override void GoTo(Building target)
+    {
+        ResetProductivity();
+        base.GoTo(target); // run method from base class
+    }
+}
